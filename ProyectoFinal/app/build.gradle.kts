14b1703ae4538b404a8416ae0eed7037bb96d8f8
Arelys Castillo <abrigitteo@uamv.edu.ni>
2026-04-29 12:37:05 -0600

    build: agregar Navigation Compose y Material Icons

--- ProyectoFinal/app/build.gradle.kts
+++ ProyectoFinal/app/build.gradle.kts
@@ -43,12 +43,13 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.navigation.compose)
+    implementation(libs.androidx.compose.material.icons.extended)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
@@ -56,13 +57,4 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
-
-    // Navigation Compose (MODERNO)
-    implementation("androidx.navigation:navigation-compose:2.7.7")
-
-    // Material 3
-    implementation("androidx.compose.material3:material3")
-
-    // Opcional pero recomendado
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 }
