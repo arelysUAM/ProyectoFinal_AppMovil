e50b86f73846ce2fe2af9d671b52f4fd6a7e6ee2
zammsara <saztaylor@uamv.edu.ni>
2026-04-29 13:40:01 -0600

    build: agregar dependencias de Navigation Compose

--- ProyectoFinal/app/build.gradle.kts
+++ ProyectoFinal/app/build.gradle.kts
@@ -55,4 +55,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+    // Navigation Compose
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
