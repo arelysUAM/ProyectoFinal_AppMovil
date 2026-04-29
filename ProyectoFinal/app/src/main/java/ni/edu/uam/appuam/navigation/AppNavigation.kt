6f91bb86d11bc851d3240f988fdbd90637e75feb
Arelys Castillo <abrigitteo@uamv.edu.ni>
2026-04-29 13:28:16 -0600

    feat: crear HomeScreen con funcionalidades básicas

--- ProyectoFinal/app/src/main/java/ni/edu/uam/appuam/navigation/AppNavigation.kt
+++ ProyectoFinal/app/src/main/java/ni/edu/uam/appuam/navigation/AppNavigation.kt
@@ -8,6 +8,7 @@ import ni.edu.uam.appuam.screens.AvatarScreen
 import ni.edu.uam.appuam.screens.HabitsScreen
 import ni.edu.uam.appuam.screens.HomeScreen
 import ni.edu.uam.appuam.screens.LoginScreen
+import ni.edu.uam.appuam.screens.TipsScreen
 
 @Composable
 fun AppNavigation() {
@@ -24,7 +25,7 @@ fun AppNavigation() {
             LoginScreen(navController)
         }
 
-        // Home con argumento (username)
+        // Home con argumento username
         composable("home/{username}") { backStackEntry ->
             val username = backStackEntry.arguments?.getString("username") ?: ""
             HomeScreen(navController, username)
@@ -36,6 +37,10 @@ fun AppNavigation() {
 
         composable("avatar") {
             AvatarScreen(navController)
+        }
+
+        composable("tips") {
+            TipsScreen(navController)
         }
     }
 }
